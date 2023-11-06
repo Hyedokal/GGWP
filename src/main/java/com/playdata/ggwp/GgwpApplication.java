a38d327f981332d4205665d16ab940a395fb7e16
Hyedokal <xcvbn5414@gmail.com>
2023-11-06 15:39:54 +0900

    initial commit

---
+++ src/main/java/com/playdata/ggwp/GgwpApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.ggwp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgwpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgwpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playdata/ggwp/GgwpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.ggwp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgwpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
