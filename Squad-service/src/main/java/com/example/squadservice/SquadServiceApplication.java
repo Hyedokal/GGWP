99251181c54c6e2b3002c7f9a1a17dc669cc0d1b
Hyedokal <xcvbn5414@gmail.com>
2023-11-07 15:34:37 +0900

    Feat: Squad-service 폴더 작성

---
+++ Squad-service/src/main/java/com/example/squadservice/SquadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.squadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquadServiceApplication.class, args);
+    }
+
+}
---
+++ Squad-service/src/test/java/com/example/squadservice/SquadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.squadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
