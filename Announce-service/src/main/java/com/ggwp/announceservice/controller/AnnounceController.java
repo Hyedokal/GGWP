258f1c494733583602ca36e74934736ab5e1ec24
Hyedokal <xcvbn5414@gmail.com>
2023-11-23 13:55:02 +0900

    Refactor: 수정사항 반영

--- Announce-service/src/main/java/com/ggwp/announceservice/controller/AnnounceController.java
+++ Announce-service/src/main/java/com/ggwp/announceservice/controller/AnnounceController.java
@@ -2,7 +2,7 @@ package com.ggwp.announceservice.controller;
 
 import com.ggwp.announceservice.dto.request.RequestAnnounceDto;
 import com.ggwp.announceservice.dto.response.ResponseAnnounceDto;
-import com.ggwp.announceservice.service.impl.AnnounceServiceImpl;
+import com.ggwp.announceservice.service.AnnounceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,40 +17,40 @@ import java.util.List;
 public class AnnounceController {
 
     @Autowired
-    private final AnnounceServiceImpl announceService;
+    private final AnnounceService announceServiceImpl;
 
     //공지사항 리스트 가져오는 메서드
     @GetMapping
     public ResponseEntity<List<ResponseAnnounceDto>> getAnnounceList() {
-        List<ResponseAnnounceDto> announceDtoList = announceService.getAllAnnounce();
+        List<ResponseAnnounceDto> announceDtoList = announceServiceImpl.getAllAnnounce();
         return ResponseEntity.ok().body(announceDtoList);
     }
 
     //공지사항 하나 가져오는 메서드
     @GetMapping("/{aId}")
     public ResponseEntity<ResponseAnnounceDto> getOneAnnounce(@PathVariable Long aId) {
-        ResponseAnnounceDto dto = announceService.getOneAnnounce(aId);
+        ResponseAnnounceDto dto = announceServiceImpl.getOneAnnounce(aId);
         return ResponseEntity.ok().body(dto);
     }
 
     //공지사항 작성하는 메서드
     @PostMapping
     public ResponseEntity<String> writeAnnounce(@RequestBody RequestAnnounceDto dto) {
-        announceService.writeAnnounce(dto);
+        announceServiceImpl.writeAnnounce(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body("Saved Successfully");
     }
 
     //공지사항 수정하는 메서드
     @PutMapping("/{aId}")
     public ResponseEntity<String> editAnnounce(@PathVariable Long aId, @RequestBody RequestAnnounceDto dto) {
-        announceService.editAnnounce(aId, dto);
+        announceServiceImpl.editAnnounce(aId, dto);
         return ResponseEntity.ok().body("Modified Successfully");
     }
 
     //공지사항 삭제하는 메서드
     @DeleteMapping("/{aId}")
     public ResponseEntity<String> deleteAnnounce(@PathVariable Long aId) {
-        announceService.deleteAnnounce(aId);
+        announceServiceImpl.deleteAnnounce(aId);
         return ResponseEntity.ok().body("Deleted Successfully");
     }
 }
