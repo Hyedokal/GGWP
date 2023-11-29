d86bb527a674e105c70771addd285c40eaac994b
kimjongha99 <jjhsun99@gmail.com>
2023-11-29 15:38:49 +0900

    Feat: 유저 상태 관리

---
+++ fronted/src/stores/index.ts
@@ -0,0 +1,5 @@
+import useUserStore from './user.store';
+
+export {
+    useUserStore
+};
---
+++ fronted/src/stores/user.store.ts
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+import LoginUser from "../types/login-user.interface";
+
+interface UserStore {
+    user: LoginUser | null;
+    setUser: (user: LoginUser | null) => void;
+}
+
+const useUserStore = create<UserStore>(set => ({
+    user: null,
+    setUser: (user: LoginUser | null) => {set((state) => ({ ...state, user }))},
+}));
+
+export default useUserStore;
