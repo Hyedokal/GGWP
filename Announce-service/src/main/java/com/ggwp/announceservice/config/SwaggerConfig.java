3d010d40c1d9702435c6b09b36e5e4b36464cffa
ifthenelse <lee.munhaeng@gmail.com>
2023-11-29 17:08:36 +0900

    swagger config 추가

---
+++ Announce-service/src/main/java/com/ggwp/announceservice/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.ggwp.announceservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("test")
+                .description("description")
+                .version("1.0.0")
+                .build();
+    }
+}
