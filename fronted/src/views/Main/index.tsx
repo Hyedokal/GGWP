7892f8f5ad288a62ce2ceb25ab777471b98d2a4a
OhJungJae <104138263+wjdwo1112@users.noreply.github.com>
2023-11-30 17:15:09 +0900

    Feat:메인페이지 프레임 수정

--- fronted/src/views/Main/index.tsx
+++ fronted/src/views/Main/index.tsx
@@ -1,17 +1,76 @@
 import './style.css';
-
-
+import { useNavigate } from 'react-router-dom';
+import { FaBell } from "react-icons/fa";
+import {MATCH_PATH, MY_PATH, SEARCH_PATH} from "../../constant";
 
 // component 인증화면 컴포넌트
 
 export default function Main() {
     // render 인증화면 컴포넌트
 
+    const navigate = useNavigate(); // useNavigate 훅 사용
+    const matchClick = () => {
+        // 버튼을 클릭했을 때 이동할 링크를 여기에 넣어주세요
+        navigate(MATCH_PATH);
+    };
+
+    const searchClick=()=>{
+        navigate(SEARCH_PATH);
+    };
+
+    const mypageClick=()=>{
+        navigate(MY_PATH);
+    }
+
     return(
         <div id='Main'>
             <div className='banner'> </div>
+            <div className='text-container'>
 
-            <div>메인화면</div>
+                <div className='img-container'>
+
+                    <div className='beggwwp'>{'GGWP에서 할 수 있어요'}</div>
+                </div>
+                <div className='logo-container'>
+                    <div> <FaBell className="FaBell" /></div>
+                </div>
+            </div>
+            <div className='function-container'>
+
+
+
+                <div className='match-box'>
+
+                    <div className='matchbox-text-box' onClick={matchClick} >
+                        <div className='matchbox-text'>{'매칭해요'}</div>
+                    </div>
+                    <div className="circle"></div>
+                    <div className='matchbox-text-box2'>
+                        사람들과 소통하며 게임해요
+                    </div>
+                </div>
+
+                <div className='search-box'>
+                    <div className='searchbox-text-box'onClick={searchClick}>
+                        <div className='searchbox-text'>{'검색해요'}</div>
+                    </div>
+                    <div className="circle"></div>
+                    <div className='matchbox-text-box2'>
+                         나의 전적을 검색해요
+                    </div>
+                </div>
+
+                <div className='mypage-box'>
+                    <div className='mypagebox-text-box'onClick={mypageClick}>
+                        <div className='mypagebox-text'>{'관리해요'}</div>
+                    </div>
+                    <div className="circle"></div>
+                    <div className='matchbox-text-box2'>
+                        내 매칭내역을 관리해요
+                    </div>
+                </div>
+            </div>
+
 
         </div>
 
