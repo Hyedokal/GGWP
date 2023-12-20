9d2af13785c52eb15ab14f0154810d368cf2fc9f
Hyedokal <xcvbn5414@gmail.com>
2023-12-20 15:44:49 +0900

    Feat: Dockerfile 추가

--- Notice-service/src/test/java/com/ggwp/noticeservice/NoticeServiceImplApplicationTests.java
+++ Notice-service/src/test/java/com/ggwp/noticeservice/NoticeServiceImplApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NoticeServiceImplApplicationTests {
-    @Test
-    void createNotice() {
-    }
+//    @Test
+//    void createNotice() {
+//    }
 }
