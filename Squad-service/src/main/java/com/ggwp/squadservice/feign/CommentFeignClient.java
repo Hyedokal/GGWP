01bf33b4f395530a12a5040de7024803da6dec8a
Hyedokal <xcvbn5414@gmail.com>
2023-12-22 16:51:18 +0900

    Fix: Feign 클라이언트 url 추가

--- Squad-service/src/main/java/com/ggwp/squadservice/feign/CommentFeignClient.java
+++ Squad-service/src/main/java/com/ggwp/squadservice/feign/CommentFeignClient.java
@@ -8,9 +8,12 @@ import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "Comment-service")
+@FeignClient(name = "Comment-service", url = "http://GGWP-LoadBalancer-298705153.ap-northeast-2.elb.amazonaws.com:80")
 public interface CommentFeignClient {
     @GetMapping("/v1/comments/search")
     Page<ResponseCommentDto> getPagedComment(@RequestBody RequestCommentPageDto.Search dto);
@@ -20,7 +23,7 @@ public interface CommentFeignClient {
 
 
     @PutMapping("/v1/comments/lolNickname-tag")
-     ResponseEntity<FeignLolNickNameTagResponseDto> FeignLolNickNameTag(@RequestBody @Valid FeignLolNickNameTagRequestDto requestBody);
+    ResponseEntity<FeignLolNickNameTagResponseDto> FeignLolNickNameTag(@RequestBody @Valid FeignLolNickNameTagRequestDto requestBody);
 
 
 }
