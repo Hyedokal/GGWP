abff7680abe925d87764f78551aae865ccfe097c
Jihoyakim <kjh970201@gmail.com>
2023-12-26 17:31:36 +0900

    Feat: 스웨거 설명 추가
    Feat: 스웨거 설명 추가

--- Search-service/src/main/java/com/ggwp/searchservice/match/controller/MatchController.java
+++ Search-service/src/main/java/com/ggwp/searchservice/match/controller/MatchController.java
@@ -6,6 +6,8 @@ import com.ggwp.searchservice.common.dto.ResponseDto;
 import com.ggwp.searchservice.match.dto.MatchDto;
 import com.ggwp.searchservice.match.dto.RequestPageDto;
 import com.ggwp.searchservice.match.service.MatchServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -19,11 +21,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/search-service/v1/match")
 @RequiredArgsConstructor
+@Tag(name = "Match", description = "Match API")
 public class MatchController {
 
     private final MatchServiceImpl matchService;
 
     @PostMapping("/create") // 매치 5개 api 불러와서 저장 (예시: 엘리스바이 KR1)
+    @Operation(summary = "Match 생성", description = "매치에 관련된 1경기에 해당하는 소환사, 리그, 매치, 참가자, 팀 모두 저장됨 -> 5번 반복 -> 총 5경기 저장")
     public ResponseDto<String> createMatch(@Valid @RequestBody FrontDto frontDto) throws JsonProcessingException {
         matchService.sendMessage(frontDto);
 //        matchService.createMatches(frontDto);
@@ -31,11 +35,13 @@ public class MatchController {
     }
 
     @PostMapping("/get")
+    @Operation(summary = "Match 조회", description = "소환사의 해당하는 경기 리스트 조회")
     public ResponseDto<List<MatchDto>> getMatchList(@Valid @RequestBody FrontDto frontDto) {
         return ResponseDto.success(matchService.getMatchList(frontDto));
     }
 
     @PostMapping("/get/pages") // 페이징 임시..
+    @Operation(summary = "Match 페이징 처리 조회", description = "페이징 처리를 통해 소환사 경기 리스트 조회: 5개 조회")
     public ResponseDto<Page<MatchDto>> getMatchPages(@Valid @RequestBody FrontDto frontDto, RequestPageDto.Search pageDto) {
         return ResponseDto.success(matchService.pagedMatches(frontDto, pageDto));
     }
