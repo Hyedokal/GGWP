01c14e9b7ec9faf3b26494187cdb573c041df704
Jihoyakim <kjh970201@gmail.com>
2023-12-26 17:31:36 +0900

    Feat: 스웨거 설명 추가
    Feat: 스웨거 설명 추가

--- Notice-service/src/main/java/com/ggwp/noticeservice/controller/AlarmController.java
+++ Notice-service/src/main/java/com/ggwp/noticeservice/controller/AlarmController.java
@@ -1,5 +1,7 @@
 package com.ggwp.noticeservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
         import org.springframework.core.io.ClassPathResource;
         import org.springframework.core.io.Resource;
@@ -14,12 +16,14 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/notice-service/v1/alarm")
+@Tag(name = "Alarm", description = "Alarm API")
 public class AlarmController {
 
     private final SimpMessageSendingOperations messagingTemplate;
 
 
     @MessageMapping("/{lolname}/{tagLine}")
+    @Operation(summary = "알람 보내기", description = "소켓을 통한 알림 메시지 보내기")
     public void message(@DestinationVariable("lolname") String lolname,@DestinationVariable("tagLine") String tagLine) {
         messagingTemplate.convertAndSend("/sub/" + lolname + "#" + tagLine, "alarm socket connection completed.");
     }
